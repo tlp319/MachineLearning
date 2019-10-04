2f0dcd3c592a33747065a415d76b0acff98bb1dc
tlp319 <tlp319@gmail.com>
2019-10-03 21:56:35 -0400

    HW2 submission
    This is my submission for hw2

---
+++ Homework 2/README.MD.txt
@@ -0,0 +1,9 @@
+The following people are in my group:
+Luke Pearson - pearsont15@students.ecu.edu (me)
+Jordance Webb - webbjo15@stduents.ecu.edu
+Toheeb Bakare - bakaret16@stduents.ecu.edu
+Thomas Gates - gatest16@students.ecu.edu
+
+$python hw2.py
+
+for k's 1-14, the best K value is 12, with an accuracy of .8842515303283249
---
+++ Homework 2/hw2.py
@@ -0,0 +1,61 @@
+# Luke Pearson
+# HW2
+# 10/3/2019
+
+from sklearn.cluster import KMeans
+from sklearn.datasets import make_blobs
+from sklearn.datasets.samples_generator import make_blobs
+from sklearn.metrics import accuracy_score
+from scipy.stats import mode
+import numpy as np
+from sklearn.metrics import confusion_matrix
+from sklearn.datasets import load_digits
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
+
+
+class K_Accuracy:
+  def __init__(self, k_val, accuracy, returnArray):
+    self.k_val = k_val
+    self.accuracy = accuracy
+    self.returnArray = returnArray
+
+
+def k_means(k):
+    digits = load_digits()
+    kmeans = KMeans(n_clusters=k, random_state=0)
+    clusters = kmeans.fit_predict(digits.data)
+    labels = np.zeros_like(clusters)
+    
+    for i in range(k):
+        mask = (clusters == i)
+        labels[mask] = mode(digits.target[mask])[0]
+
+    returnArray = [str(accuracy_score(digits.target, labels)), labels, digits]
+    return returnArray
+
+
+
+
+def main():
+    # Range of K values to test = 1 to ks
+    ks = 14
+    bestK = K_Accuracy(0, 0, None)
+    for i in range(ks):
+        currentK = K_Accuracy(i+1, k_means(i+1)[0], None)
+        # Uncomment for debugging
+        # print "K = " + str(currentK.k_val) + " has accuracy: " + str(currentK.accuracy)
+        if currentK.accuracy > bestK.accuracy:
+            bestK = currentK
+
+    bestK.returnArray = k_means(bestK.k_val)
+            
+    print "For K's in range 1 to " + str(ks) + ":\nthe best K for k-means is " + str(bestK.k_val) + " with an accuracy of " + str(bestK.accuracy)
+    
+    mat = confusion_matrix(bestK.returnArray[2].target, bestK.returnArray[1])
+    sns.heatmap(mat.T, square=True, annot=True, fmt='d', cbar=False, xticklabels=bestK.returnArray[2].target_names, yticklabels=bestK.returnArray[2].target_names)
+    plt.xlabel('true label')
+    plt.ylabel('predicted label')
+    plt.show()
+        
+main()
